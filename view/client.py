94389e69e47e70f1d292db5cfa0239d1289d65c7
monotera <monotera@pop-os.localdomain>
2021-11-14 00:31:38 -0500

    Initial DHT implementation

--- view/client.py
+++ view/client.py
@@ -1,6 +1,8 @@
 
+from ctypes import resize
 import zmq
-
+import asyncio
+from kademlia.network import Server
 
 # The subscriber thread requests messages starting with
 # A and B, then reads and counts incoming messages.
@@ -8,7 +10,6 @@ import zmq
 def subscriber_thread(opc, opc2):
     ctx = zmq.Context.instance()
 
-    opc = input("esperando filtro")
 
     # Subscribe to "A" and "B"
     subscriber = ctx.socket(zmq.SUB)
@@ -30,7 +31,29 @@ def subscriber_thread(opc, opc2):
 
     print ("Subscriber received %d messages" % count)
 
+async def validate_user(username, password):
+        server = Server()
+        await server.listen(8469)
+        bootstrap_node = ("127.0.0.1", 8469)
+        await server.bootstrap([bootstrap_node])
+
+        await server.set("usuario","pass")
+        result = await server.get(username)
+        server.stop()
+        if result == password:
+            return True
+        return False
+
 if __name__ == "__main__":
+    
+    username = input ("Ingrese su usuario: ")
+    password = input ("Ingrese su contrasena: ")
+    
+
+    while not asyncio.run(validate_user(username,password)):
+        username = input ("Ingrese su usuario: ")
+        password = input ("Ingrese su contrasena: ")
+
     opc = input ("Ingrese el primer trabajo: ")
     opc2 = input ("Ingrese el segundo trabajo: ")
     subscriber_thread(opc, opc2)
