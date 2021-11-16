6622ac3afe6f04e188a54aed294bf7f66ad30d2a
monotera <nelson.mosquera@javeriana.edu.co>
2021-11-16 00:13:44 -0500

    FailSafe

--- app/server.py
+++ app/server.py
@@ -9,35 +9,9 @@ from kademlia.network import Server
 from zmq.sugar.constants import NULL
 import Employee
 from collections import namedtuple
+from random import randrange
 
 numUser = 0
-
-async def validate_user(msg):
-    data = msg.split(" ")
-    server = Server()
-    await server.listen(8469)
-    bootstrap_node = ("127.0.0.2", 8469)
-    await server.bootstrap([bootstrap_node])
-
-    result = await server.get("Carlos")
-    print(result)
-    data2 = result.split(" ")
-    server.stop()
-
-    if data2[3] == data[3]:
-        return result
-    return NULL
-
-        
-async def add_user(msg):
-    data = msg.split(" ")
-    server = Server()
-    await server.listen(8469)
-    bootstrap_node = ("127.0.0.1", 8469)
-    await server.bootstrap([bootstrap_node])
-    await server.set("Carlos", "msg")
-    server.stop()
-    return True;
 
 def process_str(msg):
     data = msg.split(" ")
@@ -62,15 +36,13 @@ async def main():
 
     print("... Datos cargados")
 
-    
-    
     while True:
-        
+
         msg = worker.recv_multipart()
         if not msg:
             break
-
         cent = process_str(msg[2].decode())
+     
         data = msg[2].decode()
         data_split = data.split(" ")
         if(cent == "create"):
@@ -81,7 +53,7 @@ async def main():
                 anteriores = json.load(f)
 
             anteriores[f'{data_split[2]}'] = json_data['aps']
-            
+
             with open('resultado.json', 'w') as file:
                 json.dump(anteriores, file, indent=4, default=str)
 
@@ -91,20 +63,61 @@ async def main():
             res = cent.split(" ")
             if(res[3] != data_split[3]):
                 cent = False
-        else:
+            else:
+                res[-1] = data_split[-1]
+                res[-2] = data_split[-2]
+                cent = " ".join(res)
+                await server.set(data_split[2],cent)
+
+        elif cent == "offer":
             cent = ""
+            print(data_split)
+            require_job = data_split[5]
+            require_hab = data_split[6]
+            date_job = data_split[-2]
+            owner_job = data_split[-1]
             raw_values = server.storage.data.values()
             list_values = list(raw_values)
+            candidates = []
             for value in list_values:
-                value_split = value[1].split(" ")
-                if value_split[6] == "Habilidad":
-                    cent = cent + " | " + value[1]
-            oferta = msg[2].decode()
-            hab = msg[3].decode()
-            print("oferta: ",oferta)
-            print("hab: ",hab)
+                value_split = value[1].split(" ") 
+                cat1 = value_split[8][2:-2]
+                cat2 = value_split[9][1:-2]
+                
+                if value_split[1] == "0" and value_split[5] == "True" and int(data_split[3]) > 0:
+                    if value_split[6] == require_hab and (require_job == cat1 or require_job == cat2):
+                        candidates.append(value_split[2] + ","+ require_job + "," + date_job+ "," + owner_job)
+                        #cent = cent + " | " + value_split[2] + ","+ require_job + "," + date_job +" | "
+                number_vacancy = int(data_split[3])
+
+                counter = 0
+            for i in range(len(candidates)):
+                cent = cent + " | "+ candidates[i]
+                counter += 1
+                if(counter > number_vacancy):
+                    break
             
-        
+            data_split[3] = str(number_vacancy - counter)
+
+            key = data_split[-1] + "-" + data_split[2]
+            offer_data = " ".join(data_split)
+
+            await server.set(key, offer_data)
+
+            json_data = dict(aps=data_split)
+            with open('resultado.json') as f:
+                anteriores = json.load(f)
+
+            anteriores[f'{key}'] = json_data['aps']
+
+            with open('resultado.json', 'w') as file:
+                json.dump(anteriores, file, indent=4, default=str)
+
+                    
+
+            
+            
+
         msg[2] = str(cent).encode()
         print("I: (%s) normal reply" % identity)
         
@@ -125,29 +138,3 @@ worker.send_string(LRU_READY)
 
 asyncio.run(main())
 
-
-
-
-        
-"""
-
-while True:
-
-    
-    #  Wait for next request from client
-    message = socketFiltro.recv_pyobj()
-    print(f"Received request: {message}")
-
-    if(message.funct == "validate"):
-        print(message.username)
-        asyncio.run(validate_user(message.username, message.password))
-    elif(message.funct == "create"):
-        
-        asyncio.run(add_user(message.name, str(message)))
-
-        with open('resultado.json', 'w') as file:
-            json.dump(str(message), file, indent=4)
-
-        #  Send reply back to client
-        socketFiltro.send_string("OK")
-"""
